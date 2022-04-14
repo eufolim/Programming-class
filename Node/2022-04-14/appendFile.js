f045e004f74cdc024595794f718eba494bcb99df
eufolim <luiz.afc@outlook.com>
2022-04-14 09:08:43 -0300

    1.0.1

--- Node/2022-04-14/appendFile.js
+++ Node/2022-04-14/appendFile.js
@@ -1,6 +1,15 @@
+/*
 var fs = require('fs');
 
 fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
   if (err) throw err;
   console.log('Saved!');
+}); 
+*/
+
+var fs = require('fs');
+
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+  if (err) throw err;
+  console.log('Updated!');
 });
