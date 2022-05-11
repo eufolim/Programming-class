ede80823b7c07992838b6afd91563401b437f621
eufolim <luiz.afc@outlook.com>
2022-05-11 16:27:43 -0300

    1.0.7

--- Node/2022-05-06/demo_create_db.js
+++ Node/2022-05-06/demo_create_db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "aluno01"
+  password: "LemTheElf8.8"
 });
 
 con.connect(function(err) {
